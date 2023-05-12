2e92729af9844cbd201717054ef0140d8759316c
Shercosta <70560582+Shercosta@users.noreply.github.com>
2023-05-12 16:09:26 +0700

    root route ok

--- app.js
+++ app.js
@@ -11,9 +11,28 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app
+  .route("/")
+  .get((req, res) => {
+    res.render("home");
+  })
+  .post((req, res) => {
+    const data = {
+      Nama: req.body.Nama,
+      Tentang: req.body.Tentang,
+      Email: req.body.Email,
+      Nomor: req.body.Nomor,
+      Skill: req.body.Skill,
+    };
+
+    console.log(data);
+
+    res.redirect("/browse");
+  });
+
+// app.route("/browse").get((req, res) => {
+//   res.render()
+// })
 
 app.listen(3000, () => {
   console.log("Server is on http://localhost:3000");
